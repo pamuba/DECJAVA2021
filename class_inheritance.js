5bdf3a1c0a6a8a0230b6d345caf9e3a0e61e2d28
pamuba <partha.bora24@gmail.com>
2022-04-06 15:14:53 +0100

    26

---
+++ class_inheritance.js
@@ -0,0 +1,18 @@
+class A{
+    constructor(x){
+        this.a = x
+    }
+}
+class B extends A{
+    constructor(y){
+        this.b = y
+    }
+}
+class C extends B{
+    constructor(z){
+        this.c = z
+    }
+}
+
+
+///Create an object of C
