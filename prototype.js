8bbdec61d7dd79cdf1a26076a14ac82b1f37d487
pamuba <partha.bora24@gmail.com>
2022-04-06 15:03:38 +0100

    22

--- prototype.js
+++ prototype.js
@@ -1,18 +1,43 @@
-function Student(){
-    this.name = "John"
-    this.grnder = "Male"
-}
-Student.prototype.age = 99
-Student.prototype.dis = function(){
-    console.log(name+" "+gender)
-}
-//new
-//1. allocates the memory
-//2. returns the add of the allocated memory
+// function Student(){
+//     this.name = "John"
+//     this.grnder = "Male"
+// }
+// //__proto__
+// Student.prototype.age = 99
+// Student.prototype.dis = function(){
+//     console.log(name+" "+gender)
+// }
+// //new
+// //1. allocates the memory
+// //2. returns the add of the allocated memory
 
-var stdObj1  = new Student()
-// stdObj1.age = 20
-console.log(stdObj1.age)
+// var stdObj1  = new Student()
+// // stdObj1.age = 20
+// console.log(stdObj1.age)
 
-var stdObj2  = new Student()
-console.log(stdObj2.age)
+// var stdObj2  = new Student()
+// console.log(stdObj2.age)
+
+let dragon = {
+    name : "Tanya", 
+    fire : true,
+    fight(){
+        return 10
+    },
+    sing(){
+        if(this.fire){
+            return `I am ${this.name}, the breather of fire`
+        }
+    }
+}
+
+let lizard = {
+    name : "Kiki",
+    // fire : true,
+    fight(){
+        return 1
+    }
+}
+//we can borrow the sing fn for the lizard object using bind
+// const singLizard = dragon.sing.bind(lizard)
+// console.log(singLizard())
