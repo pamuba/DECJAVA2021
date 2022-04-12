c12465d54d1e12dfdaa03f74becd6d19817377f5
pamuba <partha.bora24@gmail.com>
2022-04-12 15:42:59 +0100

    51

--- NodeJS/fsDemos.js
+++ NodeJS/fsDemos.js
@@ -47,3 +47,69 @@ var fs = require('fs')
 // })
 
 /////rename a file ///////////////////
+// fs.rename('data.txt', 'new.txt', (err)=>{
+//     if(err){
+//         console.log(err.message)
+//     }
+//     else{
+//         console.log("File Written Successfully")
+//     }
+// })
+
+///delete a file////////////////////////
+// fs.unlink('new.txt', (err)=>{
+//     if(err){
+//         console.log(err.message)
+//     }
+//     else{
+//         console.log("File Written Successfully")
+//     }
+// })
+
+/////////////copy a file///////////////////////
+// var source = "data2.txt"
+// var destination = "copy.txt"
+
+// fs.copyFile(source, destination, (err)=>{
+//     if(err){
+//         console.log(err.message)
+//     }
+//     else{
+//         console.log("File Written Successfully")
+//     }
+// })
+
+
+///////copy a folder//////////////
+// var path = require('path')
+// var source = "folderA"
+// var destination = "folderB"
+
+// function copyFolder(from , to){
+//     fs.mkdirSync(to);
+//     fs.readdirSync(from).forEach(file => {
+//         fs.copyFileSync(path.join(from, file), path.join(to, file))
+//     })
+// }
+// copyFolder(source, destination)
+
+//////////////////delete a folder///////////////
+// fs.rm("folderA",{recursive:true}, (err)=>{
+//     if(err){
+//     console.log(err.message)
+//     }
+//     else{
+//         console.log("Folder removed Successfully")
+//     }
+// })
+
+
+//////truncate a file/////////////////////
+// fs.unlink('data2.txt', (err)=>{
+//     if(err){
+//         console.log(err.message)
+//     }
+//     else{
+//         console.log("File Deleted Successfully")
+//     }
+// })
